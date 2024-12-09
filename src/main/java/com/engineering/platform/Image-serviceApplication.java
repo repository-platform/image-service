53d1541dc2d84ae32e598a1377b491aa8f302c2d
Himanshu Sati <hsati@adobe.com>
2024-12-10 02:23:03 +0530

    Initial commit

---
+++ src/main/java/com/engineering/platform/Image-serviceApplication.java
@@ -0,0 +1,12 @@
+package com.engineering.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Image-serviceApplication {
+
+public static void main(String[] args) {
+SpringApplication.run(Image-serviceApplication.class, args);
+}
+}
